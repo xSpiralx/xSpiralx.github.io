5a7bd6c8a567a0f93787ee7d7035a0c74332617f
Robert McDermott <robert.mcd@live.com>
2016-06-24 17:31:20 -0600

    test

---
+++ javascript/menu_tester.js
@@ -0,0 +1,7 @@
+var menu_items = [
+    {name: "Dragonslayer", ingredients:["spring mix", "bleu patty", "onions"], image: "equal.png"},
+    {name: "Cockadoodledoo", ingredients:["chicken", "swiss cheese"], image: "higher.png"},
+    {name: "Tri tip salad", ingredients:[], image: "lower.png"}
+];
+
+var menuItem = menu_items[0].name;
